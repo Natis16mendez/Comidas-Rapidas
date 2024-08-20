86c195a4c32132a2af2c2f275894bb0850af0085
Natis16mendez <mpnatalia20@gmail.com>
2024-08-20 17:22:55 -0500

    segunda parte del trabajo

---
+++ js/index-script.js
@@ -0,0 +1,9 @@
+function cargarcabecera(){
+    fetch("/html/menu.html")
+    .then(response => response.text() )
+    .then(data => {
+        document.getElementById("cabecera-menu").innerHTML = data;
+    }).catch(error => console.log("error al cargar el html: ", error))
+}
+
+cargarcabecera();
